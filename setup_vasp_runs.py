f3b4bf625ba4877ba855889af46a4f8ad6d4858e
Ryan <rjacobs3@wisc.edu>
2017-04-20 14:08:52 -0500

    updates to setup_vasp_runs

--- setup_vasp_runs.py
+++ setup_vasp_runs.py
@@ -7,8 +7,7 @@ import logging
 import warnings
 
 def main():
-
-    directory_list = DirectoryUtilities()._get_downmost_directory_list()
+    directory_list = DirectoryUtilities().get_downmost_directory_list()
     logging.basicConfig(filename='setup_vasp_runs.log', level=logging.INFO)
 
     for directory in directory_list:
@@ -18,12 +17,13 @@ def main():
         if os.path.exists(directory+"/"+"POSCAR"):
             pfs = PotcarFileSetup(psp_type="PBE", poscar="POSCAR")
             kptset = KpointsFileSetup(mesh_type="Monkhorst-Pack", mesh_value="2 2 2")
-            incarset = IncarFileSetup(run_type="full_relax", XC_type="GGA", number_of_nodes=2, poscar="POSCAR")
+            incarset = IncarFileSetup()
             submitset = SubmitFileSetup(cluster="ACI", queue="morgan", number_of_nodes=2)
-            kptset.get_kpoints_file
-            incarset.get_incar_file
-            submitset.get_submit_file
-            pfs.get_potcar_file
+            kptset.write_kpoints_file()
+            incarset.write_predefined_incar_file(simulation_type="full_relax", xc_functional="GGA", number_of_nodes=2,
+                                                 disable_symmetry=True)
+            submitset.write_submit_file()
+            pfs.write_potcar_file()
             logging.info('Successfully created input files in directory '+directory)
         else:
             warnings.warn('Could not create POTCAR file in directory '+directory)
